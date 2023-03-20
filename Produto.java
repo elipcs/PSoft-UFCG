52613ba357034af323a4b8e1763de1a347cf1f5a
Eli Pedro <96128011+elipcs@users.noreply.github.com>
2023-03-20 10:24:11 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,11 @@
+public class Produto{
+
+private String nome;
+private String id;
+private String fabricante;
+
+public Produto(String nome, String fabricante){
+this.nome = nome;
+this.fabricante = fabricante;
+}
+}
